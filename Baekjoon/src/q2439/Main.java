2132b736718b703b843f2b0a5d5ae26ef8d7a4bb
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-08 16:56:24 +0900

    백준 2439번 문제

---
+++ Baekjoon/src/q2439/Main.java
@@ -0,0 +1,25 @@
+package q2439;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		
+		for (int i = 0; i < N; i++) {
+			for(int k = N-1; k > i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
