6491cb0d6a7233cc5e41446611a9c8ae35241d0a
Class01 <Class01@DESKTOP-CN96AQ4>
2023-05-19 14:03:16 +0900

    백준 2741번 문제

---
+++ Baekjoon/src/q2741/Main.java
@@ -0,0 +1,17 @@
+package q2741;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		for (int i = 1; i<=N; i++) {	
+			System.out.println(i);
+		}		
+		
+	}
+
+}
